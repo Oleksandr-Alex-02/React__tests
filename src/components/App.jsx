b3b24e86776bea45b1dd3da9579c74e5ebe45a95
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2022-10-28 13:39:40 +0300

    test-1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
-export const App = () => {
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
+
+import { useState, useEffect } from "react";
+
+const App = () => {
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    document.title = `You ${value} times`;
+  }, [value]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <p>You clicked {value} times</p>
+      <button onClick={() => setValue(value + 1)}>Click me +</button>
+      <button onClick={() => setValue(value - 1)}>Click me -</button>
     </div>
   );
 };
+
+
+export default App;
