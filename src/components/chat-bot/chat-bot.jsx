585013b3641827dbb018c93413cac92f141ac650
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2022-11-01 19:43:02 +0200

    n

---
+++ src/components/chat-bot/chat-bot.jsx
@@ -0,0 +1,20 @@
+class ChatBot {
+    public static void main(String[] args)
+        System.out.println("Я чат-бот");
+    }
+}
+
+public class ChatBot {
+    public static void main(String[] args) {
+        int i1 = 1;
+        int i2 = 2;
+        int iResult = i1 / i2;
+
+        float f1 = 1.0f;
+        float f2 = 2.0f;
+        //Write code under this line
+        float fResult = f1 / f2;
+    }
+}
+
+// @id:vscjava.vscode-java-pack
